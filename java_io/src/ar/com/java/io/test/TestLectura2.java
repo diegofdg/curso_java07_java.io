5e92874fb13b10406287638077a02f1fb8a1f27a
diegofdg <diegofdg@gmail.com>
2022-06-16 18:07:11 -0300

    clase_04-utilizando_java.util.scanner

---
+++ java_io/src/ar/com/java/io/test/TestLectura2.java
@@ -0,0 +1,43 @@
+package ar.com.java.io.test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TestLectura2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File("archivo.csv"));
+		String linea;
+		while (scanner.hasNextLine() && (linea = scanner.nextLine()) != null) {
+			/*			
+			// Imprimiendo el contenido
+					
+			System.out.println(linea);			
+			*/
+			
+			// Usando Split
+			
+			Object[] lineas = linea.split(",");
+			// System.out.println(lineas);
+			System.out.println(Arrays.asList(lineas));			
+			System.out.println(String.format("%s,%s,%s,%s,%s", lineas));
+			System.out.println(lineas[1]);
+						
+			/*
+			// Usando Scanner
+						
+			Scanner lineaScanner = new Scanner(linea);
+			lineaScanner.useDelimiter(",");
+			String valor;
+			while (lineaScanner.hasNextLine() && (valor = lineaScanner.nextLine()) != null) {
+				System.out.println(valor);
+			}
+			*/			
+			
+		}
+	}
+}
